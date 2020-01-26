ba3ed6254c79118bd5a1c2725779815032425b71
nikita-skobov <skobo002@umn.edu>
2020-01-25 19:08:07 -0600

    adds initial frontend files

---
+++ frontend/src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+export function App(props) {
+    return (
+        <div className="h-100">
+            hello world
+        </div>
+    )
+}
+
+
+export default App
---
+++ frontend/src/index.js
@@ -0,0 +1,20 @@
+import 'react-app-polyfill/ie9'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+
+
+import './index.css'
+import App from './components/App'
+import { setupStore } from './setupStore'
+
+const store = setupStore()
+
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root'),
+)
